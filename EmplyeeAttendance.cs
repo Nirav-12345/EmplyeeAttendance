98f345c6c793cc0440330125c3dc3aaf2f398ac4
Nirav-12345 <Nirav-12345>
2022-02-18 09:36:24 -0800

    adding Employee Ateendance class file

---
+++ EmplyeeAttendance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmplyeePresent
+{
+    public class EmplyeeAttendance
+    {
+        public static void EmpPresentOrNot()
+        {
+            Random random = new Random();
+            int number = random.Next(2);
+
+            if (number==1)
+
+            {
+                Console.WriteLine("Employee Present");
+
+
+
+
+            }
+
+            else
+            {
+                Console.WriteLine("Employee Absent");
+            }
+        }
+    }
+}
